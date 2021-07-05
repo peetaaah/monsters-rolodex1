bcc0691d45bd5155262a93905b09f6869d171dd3
peetaaah <peter@humanmanaged.com>
2021-07-05 22:35:46 +0800

    first deployment of Monster Rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./Components/card-list/card-list.component";
+import { SearchBox } from "./Components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    //extends functionality across the different methods, like the componentDidMount
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // //specific to handleChange!
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  //arrow functions automatically gets lexical binding, i.e. 'this'
+
+  render() {
+    // renders any JSX, and gains access to the 'state'
+    const { monsters, searchField } = this.state;
+    // the above deconstruction is equivalent to 'const monsters = this.state.monsters, and
+    // const searchField = this.state.searchField
+
+    // use arrow functions on any class methods you define, and aren't part of React (i,e, render(), componentDidMount())
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      // returns all the JSX as HTML
+      <div className="App">
+        <h1> Monsters Rolodex</h1>
+        <SearchBox
+          handleChange={this.handleChange}
+          placeholder="Search Monsters..."
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
